d2eb72cbc47da6e5d7ceb1a0bd85685183097a73
Ma Re <Ma Re>
2017-02-21 13:45:34 -0500

    init dashboard config

---
+++ src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  templateUrl: './dashboard.component.html',
+  styleUrls: [ './dashboard.component.css' ]
+})
+export class DashboardComponent {}
