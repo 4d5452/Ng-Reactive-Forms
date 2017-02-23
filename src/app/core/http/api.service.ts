01995cda4121721b0d02bfac87a670ce1702ca67
Ma Re <Ma Re>
2017-02-22 19:14:21 -0500

    more comments

--- src/app/core/http/api.service.ts
+++ src/app/core/http/api.service.ts
@@ -9,26 +9,27 @@ export class ApiService {
   private API_ROOT = 'api'; // URL to web api
   
   constructor(private http: Http, private requestOptions: RequestOptionsService) { }
-  /**getAll: returns array of all objects contained in collection */
+  /**getAll: returns all items from server db collection */
   getAll(collection: string): Observable<Response> {
     return this.http.get(`${this.API_ROOT}/${collection}`, this.requestOptions.method(RequestMethod.Get))
       .map((response: Response) => response);
   }
+  /**get: returns single item from server db collection */
   get(collection: string, id: number): Observable<Response> {
     return this.http.get(`${this.API_ROOT}/${collection}/${id}`, this.requestOptions.method(RequestMethod.Get))
       .map((response: Response) => response);
   }
-  /**Add Item: return response in json */
+  /**add: add single item to server db collection */
   add(collection: string, body: Object): Observable<Response> {
     return this.http.post(`${this.API_ROOT}/${collection}`, JSON.stringify(body), this.requestOptions.method(RequestMethod.Post))
       .map((response: Response) => response);
   }
-  /**Remove Item */
+  /**remove: removes item from server db  collection */
   remove(collection: string, id: number): Observable<Response> {
     return this.http.delete(`${this.API_ROOT}/${collection}/${id}`, this.requestOptions.method(RequestMethod.Delete))
       .map((response: Response) => response);
   }
-  /**Update Item */
+  /**update: creates or updates item on server db collection */
   update(collection: string, id: number ,body: Object): Observable<Response> {
     return this.http.put(`${this.API_ROOT}/${collection}/${id}`, JSON.stringify(body), this.requestOptions.method(RequestMethod.Put))
       .map((response: Response) => response);
