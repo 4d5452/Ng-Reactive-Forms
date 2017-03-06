d9428055389401369f6120574cf4742adf3629e1
Ma Re <Ma Re>
2017-03-06 16:01:29 -0500

    ...

--- src/app/store/models/table.models.ts
+++ src/app/store/models/table.models.ts
@@ -1,9 +1,9 @@
 import { type } from '../../shared/util';
 
 export interface ColumnMetaObject {
-  column: string;
-  type: string;
-  selector: string;
+  column: string; //Name displayed in table header
+  type: string; //Determines format of data in column
+  selector: string; //Data object selector: i.e. item[selector]
 }
 
 export const SortOrder = {
