f3ffde9e60f8cc41b54003a08b7473444896d206
Ma Re <Ma Re>
2017-03-19 21:59:06 -0400

    ...

--- src/app/records/records-modify.component.ts
+++ src/app/records/records-modify.component.ts
@@ -5,6 +5,9 @@ import { Subscription} from 'rxjs/Subscription';
 
 import { Filter, CleaningRecord } from '../store/models/app.models';
 
+import { memberOfValidator } from '../shared/member-of';
+import { isUniqueValidator } from '../shared/is-unique';
+
 import { CollectionService } from '../core/collection.service';
 import { PopupService } from '../core/popup.service';
 
@@ -15,38 +18,109 @@ import { PopupService } from '../core/popup.service';
 })
 export class RecordsModifyComponent implements OnInit, OnDestroy{
   formGroup: FormGroup;
-  popupTask$: Observable<string>;
-
-  filters$: Observable<Filter[]>;
-
-  record$: Subscription;
-  record: CleaningRecord;
+  task: string;
+  filters: string[];
+  records: string[];
+  record: CleaningRecord;  
+  formErrors = {
+    id: '',
+    filter: '',
+    currentEquipmentMeter: '',
+    pre: '',
+    post: '',
+    cycles: ''
+  };
+  validationMessages = {
+    'id': {
+      'required': 'ID is required',
+      'isUnique': 'ID is not unique'
+    },
+    'filter': {
+      'required': 'Filter ID is required',
+      'memberOf': 'Filter does not exist'
+    },
+    'currentEquipmentMeter': {
+      'required': 'Current equipment meter is required',
+    },
+    'pre': {
+      'required': 'Pre clean value is required',
+    },
+    'post': {
+      'required': 'Post clean value is required',
+    },
+    'cycles': {
+      'required': '# of cycles is required',
+    }
+  };
+  valueChanges$: Subscription;
 
   constructor(private fb: FormBuilder, private collectionService: CollectionService,
     private popupService: PopupService) {
-    this.popupTask$ = this.popupService.getPopupTask();
-    this.filters$ = this.collectionService.getCollection('filters');
   }
   
   ngOnInit() {
-    this.record$ = this.getRecord().subscribe((record)=> {
-      this.record = record;
-    });
+    this.popupService.getPopupTask()
+      .subscribe((task)=>this.task=task).unsubscribe();
+
+    this.ids('records')
+      .subscribe((records)=>this.records=records)
+      .unsubscribe();
+
+    this.ids('filters')
+      .subscribe((filters)=>this.filters=filters)
+      .unsubscribe();
+
+    this.getRecord()
+      .subscribe((record)=> {
+        this.record = record;
+      }).unsubscribe();
+
     this.createForm();
   }
   ngOnDestroy() {
-    this.record$.unsubscribe();
+    this.valueChanges$.unsubscribe();
   }
 
   createForm() {
     this.formGroup = this.fb.group({
-      id: [this.record['id'], Validators.required ],
-      filter: [this.record['filter'], Validators.required ],
-      currentEquipmentMeter: [this.record['currentEquipmentMeter'], Validators.required ],
-      pre: [this.record['pre'], Validators.required ],
-      post: [this.record['post'], Validators.required ],
-      cycles: [this.record['cycles'], Validators.required ],
+      id: [
+        this.record['id'], 
+        Validators.compose([
+          Validators.required,
+          isUniqueValidator(this.records)
+        ])
+      ],
+      filter: [
+        this.record['filter'], 
+        Validators.compose([
+          Validators.required,
+          memberOfValidator(this.filters)
+        ])
+      ],
+      currentEquipmentMeter: [
+        this.record['currentEquipmentMeter'], 
+        Validators.compose([
+          Validators.required
+        ])
+      ],
+      pre: [
+        this.record['pre'], 
+        Validators.required 
+      ],
+      post: [
+        this.record['post'], 
+        Validators.required 
+      ],
+      cycles: [
+        this.record['cycles'], 
+        Validators.required 
+      ],
     });
+
+    this.valueChanges$ = this.formGroup.valueChanges
+      .subscribe(data=> this.onValueChanged(data));
+
+    this.onValueChanged();
   }
 
   onSubmit(): void {
@@ -72,12 +146,32 @@ export class RecordsModifyComponent implements OnInit, OnDestroy{
     }
     return saveModel;
   }
+  onValueChanged(data?: any): void {
+    if(!this.formGroup) { return; }
+    const form = this.formGroup;
+
+    for(const field in this.formErrors) {
+      // clear previous error message (if any)
+      this.formErrors[field] = '';
+      const control = form.get(field);
+      if(control && control.dirty && !control.valid) {
+        const messages = this.validationMessages[field];
+        for(const key in control.errors) {
+          this.formErrors[field] += messages[key] + ' ';
+        }
+      }
+    }
+  }
   getRecord(): Observable<CleaningRecord> {
-    return this.popupTask$
-      .switchMap((task)=> {
-        return task==='edit' ? this.collectionService.getSelectedItem() : Observable.of(<CleaningRecord>{
-          id: '', filter: '', currentEquipmentMeter: null, pre: null, post: null, cycles: null, created: Date.now(), modified: Date.now()
-        })
+    return this.task==='edit' ? this.collectionService.getSelectedItem() : Observable.of(<CleaningRecord>{
+      id: '', filter: '', currentEquipmentMeter: null, pre: null, post: null, cycles: null, created: Date.now(), modified: Date.now()
+    })
+  }
+
+    ids(collection: string): Observable<string[]> {
+    return this.collectionService.getCollection(collection)
+      .map((types)=>{
+        return types.map((val)=>val['id']);
       })
   }
 }
